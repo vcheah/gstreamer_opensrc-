11e8a5a673d088e18f1719428f3c5e360409d29c
Cheah, Vincent Beng Keat <vincent.beng.keat.cheah@intel.com>
2026-08-19 20:04:45 +0800

    add debug-print

--- subprojects/gst-plugins-bad/sys/va/gstvabase.c
+++ subprojects/gst-plugins-bad/sys/va/gstvabase.c
@@ -107,12 +107,16 @@ _try_import_buffer (GstVaBufferImporter * importer, GstBuffer * inbuf)
 
   surface = gst_va_buffer_get_surface (inbuf);
   if (surface != VA_INVALID_ID &&
-      (gst_va_buffer_peek_display (inbuf) == importer->display))
+      (gst_va_buffer_peek_display (inbuf) == importer->display)) {
+    GST_ERROR("[bkcheah][%s] dmabuf/vaMemory = same-vadpy", __func__);
     return TRUE;
+  }
 
   g_rec_mutex_lock (&GST_VA_SHARED_LOCK);
   ret = _try_import_dmabuf_unlocked (importer, inbuf);
 
+  if (ret)
+    GST_ERROR("[bkcheah][%s] dmabuf/vaMemory = diff-vadpy >>>>", __func__);
   g_rec_mutex_unlock (&GST_VA_SHARED_LOCK);
 
   return ret;
